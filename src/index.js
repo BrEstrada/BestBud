5628c763e6731bfbbc6634eda36e5dc912e7922e
Brandon_Estrada <65381980+BrEstrada@users.noreply.github.com>
2022-03-11 22:38:33 -0700

    basic login page, 2 col layout with image

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { GlobalStyles } from './styles/GlobalStyles';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 
 ReactDOM.render(
--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+import { AppBar } from './../../components/appbar';
+import LoginBanner from './../../assets/images/login-banner.png';
 
 function LoginPage(props) {
+    let navigation = useNavigate();
+
+    // JS
+    function onHandleSubmit(e) {
+        e.preventDefault();
+        // check the inputs email and password
+        // firebase auth signInWithEmailAndPassword
+        // true -> Dashboard
+        // false -> resubmit
+        navigation('/dashboard');
+    }
     return (
         <>
+            <AppBar />
+
             <nav>
                 <ul>
                     <li>
@@ -14,9 +32,42 @@ function LoginPage(props) {
                     </li>
                 </ul>
             </nav>
-            <header>
-                <h1>Login Page</h1>
-            </header>
+
+            <Container>
+                <Row className="mt-5">
+                    <Col className="bg-dark text-light p-4">
+                        <h1 className="fs-1">Welcome to Best Bud,</h1>
+                        <p className="fs-3 mb-3">Please Login.</p>
+                        <img alt="" src={LoginBanner} />
+                    </Col>
+                    <Col className="bg-light py-5">
+                        <Form onSubmit={onHandleSubmit}>
+                            <Form.Group className="mb-3" controlId="email">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control
+                                    id="email"
+                                    type="email"
+                                    placeholder="Email@example.com"
+                                    required
+                                />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    id="password"
+                                    type="password"
+                                    placeholder="Password"
+                                    required
+                                />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
